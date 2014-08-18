f843cb5b79c0f0fe3a69c08dbccccaf02d376670
Comodoro <Comodoro@192.168.1.105>
2014-08-18 18:49:57 -0500

    administracion

--- administracion/guardarImagen.php
+++ administracion/guardarImagen.php
@@ -1,6 +1,6 @@
 <?php
 
-$target_path = "uploads/";
+$target_path = "../index/imgs-slider/";
 $target_path = $target_path . basename($_FILES['uploadedfile']['name']);
 if (move_uploaded_file($_FILES['uploadedfile']['tmp_name'], $target_path)) {
     echo "El archivo " . basename($_FILES['uploadedfile']['name']) . " ha sido subido";
